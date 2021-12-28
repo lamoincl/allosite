eccdb760401bb65c56de7007c84955b7851ba61a
clement.lamoine <clement.lamoine@mines-ales.org>
2021-12-28 16:31:15 +0100

    ADD Acceuil page ADD Allos page ADD The logo in favicon.png ADD css and js files w3 and bootstrap dependencies ADD local css MOD app.py to add all theses pages

--- app.py
+++ app.py
@@ -1,11 +1,32 @@
-from flask import Flask
+from flask import *
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+def accueil():
+    context = {"sitename": ""}
+    return render_template('base.html')
+
+
+@app.route('/allos')
+def allos():
+    return render_template('allos/allos.html')
+
+
+@app.route('/allos/allo-crepe')
+def allo_crepe():
+    return render_template('allos/allocrepe.html')
+
+
+@app.route('/allos/allo-la-terre-ici-ma-lune')
+def allo_la_terre_ici_ma_lune():
+    return render_template('allos/allolaterreicimalune.html')
+
+
+@app.route('/presentation')
+def presentation():
+    return render_template('presentation.html')
 
 
 if __name__ == '__main__':
