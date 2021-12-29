42f120033f047bf10fd74409286f1251ebb7dbd9
clement.lamoine <clement.lamoine@mines-ales.org>
2021-12-29 16:02:04 +0100

    Change project structure

--- app.py
+++ app.py
@@ -5,8 +5,12 @@ app = Flask(__name__)
 
 @app.route('/')
 def accueil():
-    context = {"sitename": ""}
     return render_template('base.html')
+
+
+@app.route('/suivi')
+def suivi():
+    pass
 
 
 @app.route('/allos')
@@ -19,8 +23,12 @@ def allo_crepe():
     return render_template('allos/allocrepe.html')
 
 
-@app.route('/allos/allo-la-terre-ici-ma-lune')
+@app.route('/allos/allo-la-terre-ici-ma-lune', methods=['GET', 'POST'])
 def allo_la_terre_ici_ma_lune():
+    if request.method == 'POST':
+        fb = request.form['id_fb']
+        appart = request.form['appart']
+        print(fb, appart)
     return render_template('allos/allolaterreicimalune.html')
 
 
